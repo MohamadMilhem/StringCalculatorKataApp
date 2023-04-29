2a6046918c2b2df898ae0d0fe61a2735d97706d4
Mohammad Milhem <mohamadmilhem100@gmail.com>
2023-04-29 22:54:51 +0300

    Added more failing tests

--- StringCalculatorKata.Tests/StringCalculatorTests.cs
+++ StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -15,5 +15,37 @@ namespace StringCalculatorKata.Tests
             Assert.Equal(0, result);
 
         }
+
+        [Fact]
+        public void ShouldReturnZeroIfInputIsEmpty()
+        {
+            // Arrange
+            var Calculator = new StringCalculator();
+
+            // Act
+            var result = Calculator.Add("");
+
+            // Assert
+            Assert.Equal(0, result);
+
+        }
+
+        [Theory]
+        [InlineData("1")]
+        [InlineData("123")]
+        public void ShouldReturnTheSameNumberIfInputOnlyOneNumber(string input)
+        {
+            // Arrange
+            var Calculator = new StringCalculator();
+            var expected = Int32.Parse(input);
+
+            // Act
+            var result = Calculator.Add(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+
     }
 }
